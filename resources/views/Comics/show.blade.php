8cb7cf01f0eec78eb7c3486315c99212a0ee7e61
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-19 13:02:53 +0100

    feat: delete button

--- resources/views/Comics/show.blade.php
+++ resources/views/Comics/show.blade.php
@@ -21,6 +21,14 @@
             class="btn btn-warning px-4">
             Clicca per modificare il fumetto
         </a>
+
+        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+            @csrf
+            @method("DELETE")
+            <button type="submit" class="btn btn-danger px-4">
+                Clicca per eliminare il fumetto
+            </button>
+        </form>
 </div>
 
 @endsection
