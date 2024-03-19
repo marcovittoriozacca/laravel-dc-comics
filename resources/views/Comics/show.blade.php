b06b4a0543d6486e2737aad45ca3cb264cfc160b
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-19 15:19:53 +0100

    feat: Delete comic confirm with modal

--- resources/views/Comics/show.blade.php
+++ resources/views/Comics/show.blade.php
@@ -23,13 +23,39 @@
         Clicca per modificare il fumetto
     </a>
 
-    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-        @csrf
-        @method("DELETE")
-        <button type="submit" class="btn btn-danger px-4">
-            Clicca per eliminare il fumetto
-        </button>
-    </form>
+
+
+    <!-- Modale conferma eliminazione fumetto -->
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteComicModal">
+    Elimina Fumetto
+    </button>
+  
+    <div class="modal fade" id="deleteComicModal" tabindex="-1" aria-labelledby="deleteComicModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="deleteComicModalLabel">Elimina Fumetto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h4 class="text-danger">
+                        Sei sicuro di voler eliminare questo fumetto? Non potrai recuperarlo in alcun modo.
+                    </h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-danger">
+                            Elimina
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  
 </div>
 
 @endsection
